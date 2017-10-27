805ae5ec6641979b678805f59a76db6e77899824
David Watkins <djrival7@gmail.com>
2017-10-26 20:00:11 -0400

    Revamped UI and uses roslaunch instead of command line call

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,39 +1,56 @@
 #!/usr/bin/env python
 import Tkinter as tk
-import subprocess as sub
-from PIL import Image, ImageTk
-import rospy
-
-dim = "250x100"
-
-root = tk.Tk()
-root.geometry(dim)
-
-alexa_cmd = 'python ~/ros/bci_project/graspit_bci_ws/src/graspit_alexa_controller/scripts/graspit_alexa_controller.py'
-switch_cmd = 'python ~/ros/bci_project/graspit_bci_ws/src/graspit_switch_controller/scripts/switch_controller.py'
-bci_cmd = '~/ros/bci_project/graspit_bci_ws/src/graspit_threshold_controller/scripts/run.sh'
-
-def toggle_experiment_type():
-	if rospy.has_param('experiment_type'):
-		experiment_type = rospy.get_param('experiment_type')
-	else:
-		experiment_type = "block"
-
-	if experiment_type == "block":
-		experiment_type = "object"
-	else:
-		experiment_type = "block"
-
-	rospy.set_param("experiment_type", experiment_type)
-
-tk.Button(root, text="Open Switch Interface", command=lambda: sub.call(switch_cmd, shell=True)).pack()
-# tk.Button(root, text="Open Echo Interface", command=lambda: sub.call(alexa_cmd, shell=True)).pack()
-tk.Button(root, text="Open BCI Interface", command=lambda: sub.call(bci_cmd, shell=True)).pack()
-tk.Button(root, text="Change Experiment Type", command=toggle_experiment_type).pack()
+import roslaunch
 
 
-img = Image.open('icon.png')
-pic = ImageTk.PhotoImage(img)
-root.tk.call('wm', 'iconphoto', root._w, pic)
-root.title("Input Command Center")
-root.tk.mainloop()
+class RunExperimentGUI:
+
+    def __init__(self):
+        self.dimensions = "250x100"
+
+        self.switch_controller_node = roslaunch.core.Node("graspit_switch_controller", "switch_controller.py")
+        self.threshold_controller_node = roslaunch.core.Node("graspit_threshold_controller", "run.sh")
+        self.launcher = roslaunch.scriptapi.ROSLaunch()
+        self.launcher.start()
+
+        self.__mainWindow = tk.Tk()
+        self.__mainWindow.geometry(self.dimensions)
+
+        self.process = None
+        self.process_name = ""
+
+        self.current_process_label = tk.Label(self.__mainWindow, text="Current process: ")
+        self._update_process_text()
+
+        tk.Button(self.__mainWindow, text="Run Ultimate Switch Node", command=self._launch_switch_ui).pack()
+        tk.Button(self.__mainWindow, text="Run sEMG Interface Node", command=self._launch_threshold_ui).pack()
+        tk.Button(self.__mainWindow, text="Close current node", command=self._close_current_process).pack()
+
+        self.__mainWindow.title("Input Command Center")
+        self.__mainWindow.tk.mainloop()
+
+    def _launch_switch_ui(self):
+        self.process = self.launcher.launch(self.switch_controller_node)
+        self.process_name = "Switch Controller"
+        self._update_process_text()
+
+    def _launch_alexa_ui(self):
+        pass
+
+    def _close_current_process(self):
+        if self.process and self.process.is_alive():
+            self.process.stop()
+
+        self.process_name = "None"
+        self._update_process_text()
+
+    def _launch_threshold_ui(self):
+        self.process = self.launcher.launch(self.threshold_controller_node)
+        self.process_name = "sEMG Controller"
+        self._update_process_text()
+
+    def _update_process_text(self):
+        self.current_process_label.config(text="Current process: {}".format(self.process_name))
+
+if __name__ == '__main__':
+    RunExperimentGUI()
